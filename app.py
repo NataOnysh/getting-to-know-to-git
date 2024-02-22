d7ccbc5e78aeceec72a2d4d8f0f6af2ae7e630bc
unknown <onyshchuk.natalie@gmail.com>
2024-02-23 01:46:42 +0200

    First commit. Calculator +-

---
+++ app.py
@@ -0,0 +1,13 @@
+def calculate(num1, operator, num2):
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    return result
+
+
+num1 = float(input('Enter first number: '))
+operator = input('Enter operator (+,-): ')
+num2 = float(input('Enter second number: '))
+
+print(f"{num1} {operator} {num2} = {calculate(num1, operator, num2)}")
