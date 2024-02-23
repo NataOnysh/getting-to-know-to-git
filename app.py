e4c64248616fb054d870bc3ea983e353dbd74070
unknown <onyshchuk.natalie@gmail.com>
2024-02-23 02:19:36 +0200

    Third commit. Calculator +-*/

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ def calculate(num1, operator, num2):
         result = num1 + num2
     elif operator == '-':
         result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        result = num1 / num2
     return result
 
 
 num1 = float(input('Enter first number: '))
-operator = input('Enter operator (+,-): ')
+operator = input('Enter operator (+,-,*,/): ')
 num2 = float(input('Enter second number: '))
 
 print(f"{num1} {operator} {num2} = {calculate(num1, operator, num2)}")
